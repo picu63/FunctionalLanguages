1c9f258ff97aae0daf8dc1ef07f412361b5c1089
Piotr Olearczyk <picu63@gmail.com>
2021-04-10 08:51:39 +0200

    finish Zad2

---
+++ LearningFSharpTutorialYT/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open FParsec
+
+let a = pstring "Hello"
+let b = pstring "World"
+let p = a<|>b
+let result = run p "Hello"
+printfn "%O" result
---
+++ Zad2/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+let isBalanced str = 
+    let rec loop leftStr stack = 
+        match (leftStr, stack) with
+        | '(' :: frstSkipped,  stack         -> loop frstSkipped ('(' :: stack)
+        | ')' :: frstSkipped, '(' :: stack   -> loop frstSkipped stack
+        | ')' :: _, _               -> false
+        | _ :: frstSkipped, stack   -> loop frstSkipped stack
+        | [], []                    -> true
+        | [], _                     -> false
+    loop (Seq.toList str) []
+
+[<EntryPoint>]
+let main argv =
+    printfn "Rozwiązanie zadania nr 31:"
+    printfn "Test 1: \"()\" - %O" (isBalanced "()")
+    printfn "Test 2: \"()()()\" - %O" (isBalanced "()()()")
+    printfn "Test 3: \"((()))\" - %O" (isBalanced "((()))")
+    printfn "Test 4: \"()((())())\" - %O" (isBalanced "()((())())")
+    printfn "Podany argument \"%O\"" argv.[0]
+    let isBalancedGiven = isBalanced argv.[0]
+    printfn "Czy jest poprawny?: %O" isBalancedGiven
+    0
